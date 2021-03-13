59e983f109b44d3a4904d5122e173d7236de58a3
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-03-13 11:13:34 +0400

    (feat): added blueprint for ttt game

---
+++ src/tic_tac_toe.py
@@ -0,0 +1,29 @@
+class Game:
+	def __init__(self, n, m):
+		self.n = n
+		self.m = m
+		self.board = # n by m empty grid
+	
+	def make_move():
+		# --- #
+
+	def winning_checker():
+		# -- #
+
+	def get_possible_actions():
+		# -- #
+
+	def max_value():
+		# -- #
+
+	def min_value():
+		# -- #
+
+	def evaluate():
+		# -- #
+
+	def alpha_beta():
+		# -- #
+
+
+# useful resource: https://github.com/Eyasluna/CSCI_6511_AI_spring2020/blob/master/project_2/tictactoe.py
