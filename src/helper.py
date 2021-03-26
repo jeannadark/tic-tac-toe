eade450275be281a857d58841a926f7b6c6d8299
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-03-27 00:58:38 +0400

    minor fix

--- src/helper.py
+++ src/helper.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 def kth_diag_indices(a, k):
-	"""Get diagonal indices using an offset.
-	
-	:param a: given array
-	:type a: array
-	:param k: offset
-	:type k: int
-	:return: tuple of rows and cols indices for diagonals
-	:rtype: tuple
-	"""
+    """Get diagonal indices using an offset.
+    
+    :param a: given array
+    :type a: array
+    :param k: offset
+    :type k: int
+    :return: tuple of rows and cols indices for diagonals
+    :rtype: tuple
+    """
     rows, cols = np.diag_indices_from(a)
     if k < 0:
         return rows[-k:], cols[:k]
