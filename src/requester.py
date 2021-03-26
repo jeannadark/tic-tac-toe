0cebe022a265767277319fefaaa7bb427c2f86a8
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-03-26 18:09:11 +0400

    fix

--- src/requester.py
+++ src/requester.py
@@ -59,7 +59,7 @@ def make_a_move(game_id: int, move: tuple):
     # print(ast.literal_eval(data.decode('utf-8')))
 
 
-def get_move_list(game_id: int, count: int = 2):
+def get_move_list(game_id: int, count: int = 1):
     """Get list of recent moves made for the game instance. Defaults to 2 recent moves."""
     get_moves_url = (
         "/aip2pgaming/api/index.php?type=moves&gameId="
