4be3aa62328cb2852618d1281668188957c3ac74
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-03-26 19:52:50 +0400

    fixes

--- src/tic_tac_toe_v2.py
+++ src/tic_tac_toe_v2.py
@@ -290,9 +290,12 @@ def play_game(opponent_team_id: int, n: int, m: int, game_id: int, player: str):
         game.draw_board()
         if game.is_end_of_game(max_depth, game.curr_board_state):
             break
+        time = 0
         while req.get_move_list(game_id)["moves"] == moves:
-            print(req.get_move_list(game_id)["moves"])
-            time.sleep(1)
+            print(time)
+            time += 1
+            if time > 10:
+                break
         move = req.get_move_list(game_id)["moves"][0]
         symbol = move["symbol"]
         x = int(move["move"].split(",")[0])
