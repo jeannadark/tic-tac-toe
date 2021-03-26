b6a5fa685bbc542754e5670ca837694b51832a86
Narmin Jamalova <65672621+njamalova@users.noreply.github.com>
2021-03-26 11:03:11 +0400

    add helper

---
+++ src/helper.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def kth_diag_indices(a, k):
+    rows, cols = np.diag_indices_from(a)
+    if k < 0:
+        return rows[-k:], cols[:k]
+    elif k > 0:
+        return rows[:-k], cols[k:]
+    else:
+        return rows, cols
